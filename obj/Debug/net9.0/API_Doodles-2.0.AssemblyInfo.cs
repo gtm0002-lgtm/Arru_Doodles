a3db26dda6945c00fcc8cd0fb6cfe2ba59139899
GerarddeTena <gerard@bcandorra.com>
2025-10-06 23:11:19 +0200

    TryCath V2

--- obj/Debug/net9.0/API_Doodles-2.0.AssemblyInfo.cs
+++ obj/Debug/net9.0/API_Doodles-2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8151539d382c505d72d1442414c6ce8c381fcc5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f4689ecfaaedebd467f63ae499385c12ecfa6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
