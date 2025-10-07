6daa9334707a40bca7a837d436de966a2fa7ab34
GerarddeTena <gerard@bcandorra.com>
2025-10-07 09:52:03 +0200

    Started EF Migration & Database Update: Checked as Correct and running OK

--- obj/Debug/net9.0/API_Doodles-2.0.AssemblyInfo.cs
+++ obj/Debug/net9.0/API_Doodles-2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c0564ffd6c2901bf35a599fc175077f1aa7b62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb07185de7715b59f61a2c0189fb8f7a87e672f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Doodles-2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
